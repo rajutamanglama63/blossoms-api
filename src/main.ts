484ff93553d5d6ba1ba6817cf686195fb6a6ad2a
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-11-01 18:08:39 +0545

    feat: cors added

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { Logger } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.use(cors());
 
   const port = process.env.PORT ? Number(process.env.PORT) : 4000;
 
